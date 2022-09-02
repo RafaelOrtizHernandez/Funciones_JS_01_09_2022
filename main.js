915f0a1988cd039a5a5a6f14051a01b55e937257
RafaelOrtizHernandez <rortiz@vanguardia.co>
2022-09-01 21:57:03 -0500

    Fórmula para hallar Área y Perímetro Círculo

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 
 
-let P = Number(prompt("Ingrese el valor del Perímetro", 4));
-let a = Number(prompt("Ingrese el valor de la apotema", 5));
-function formulaAreaPoligono(perimetro, apotema, imagen="imagenes/pregular.png"){
+let r = Number(prompt("Ingrese el valor del radio", 2));
+function formulaAreaCirculo(radio, imagen="imagenes/circulo.png"){
     document.body.insertAdjacentHTML("afterbegin", `<img src="${imagen}">`);
-    return (perimetro/2)*apotema;
+    return Math.PI*r*r;
 }
-console.log(`El Area del Poligono Regular es: ${formulaAreaPoligono(P, a)}`);
+function formulaPerimetroCirculo(radio){
+    return 2*Math.PI*r;
+}
+console.log(`El Area del Circulo es: ${formulaAreaCirculo(r)}`);
+console.log(`El Perimetro del Circulo es: ${formulaPerimetroCirculo(r)}`);
